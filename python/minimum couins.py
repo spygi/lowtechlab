695b1e6f641e7897373001870294d4e48ed082c5
Khalilmahmoodiyan <khalilmahmoodiyan@gmail.com>
2019-10-14 19:14:13 +0300

    now push

--- python/minimum couins.py
+++ python/minimum couins.py
@@ -1,7 +1,7 @@
 # to male a methode for solving the minimumchangecouins
 couins = [1,2,5,10,20,50,100,200]
 couins.reverse() # 200, 100, 50 ...
-change = -1
+change = 85
 couinsreturned = []
 for money in couins:
           while money <= change:
